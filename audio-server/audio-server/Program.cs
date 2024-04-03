ddb1da817082edadbb42f54039d53394a81353a4
Catalin <123420686+catalinsx@users.noreply.github.com>
2024-04-03 23:02:22 +0300

    Add files via upload

---
+++ audio-server/audio-server/Program.cs
@@ -0,0 +1,99 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using TagLib;
+
+namespace audio_server
+{
+	internal class Program
+	{
+        static TCPServer tcpServer = new TCPServer();
+		static void Main(string[] args)
+		{
+			
+
+
+			tcpServer.Start();
+			tcpServer.DataReceived += TcpServer_DataReceived;
+			Console.WriteLine("Press any key to stop the server...");
+			Console.ReadLine();
+			
+
+			tcpServer.Stop();
+		}
+
+		private static void TcpServer_DataReceived(string clientId, string data)
+		{
+			data = data.Replace("\r", "").Replace("\n", "");
+
+			if (data.StartsWith("<PLAY>"))
+			{
+				// extragem numele piesei din mesaju trimis de client, scoatem <play> si <eos>
+				string songName = data.Replace("<PLAY>", "").Replace("<EOS>", "");
+				Console.WriteLine($"Sending song: {songName}");
+
+				//  mergem in folderu songs si luam piesa respectiva si-o trasformam intr un byte array
+				string filePath = @$"songs\{songName}.mp3";
+				byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+
+				// encodam arrayu intr un string base64 pe care ulterior il trimitem la client si clientru
+				// o sa fac pasii opusi, primeste stringu encodat, il face byte array, il face fisier si dupa ii da play
+				string base64String = Convert.ToBase64String(fileBytes);
+
+				// trimitem stringu in chunkuri de cate 8096 bytes, trimitem la inceput <PLAY> si la final \r\n, ca sa detectam in client datele
+				int chunkSize = 1048576;
+				int bytesSent = 0;
+				while (bytesSent < base64String.Length)
+				{
+					int remainingBytes = base64String.Length - bytesSent;
+					int bytesToSend = Math.Min(remainingBytes, chunkSize);
+					string chunk = "<PLAY>" + base64String.Substring(bytesSent, bytesToSend) + "\r\n";
+					byte[] buffer = Encoding.UTF8.GetBytes(chunk);
+                    tcpServer.Send(clientId, chunk);
+					bytesSent += bytesToSend;
+				}
+
+                // la final trimitem <PLAY><EOS> ca clientu sa stie ca toate chunkurile au venit si poate face urmatorii pasi
+                tcpServer.Send(clientId, "<PLAY><EOS>");
+			}
+			if (data.StartsWith("<LIST>")) //  luam toate fisierele din folderu songs, facem un string din ele, eg. Killa - Fetele palide;Nicolae Guta- Campion;
+			{                                // si in client cand le primim punem un Split(";") si le putem adauga intr o lista sau ceva
+				string strExePath = System.Reflection.Assembly.GetExecutingAssembly().Location;
+				string strPath = Path.GetDirectoryName(strExePath) + @"\songs";
+
+				string songs = "";
+                if (Directory.Exists(strPath))
+                {
+                    string[] songFiles = Directory.GetFiles(strPath);
+                    foreach (string songFile in songFiles)
+                    {
+                        string eyo = Path.GetFileName(songFile).Replace(".mp3", "");
+                        string title = "";
+                        string artist = "";
+                        string album = "";
+                        string genre = "";
+
+                        try
+                        {
+                            var tagFile = TagLib.File.Create(songFile);
+                            title = tagFile.Tag.Title ?? "";
+                            artist = tagFile.Tag.FirstPerformer ?? "";
+                            album = tagFile.Tag.Album ?? "";
+                            genre = tagFile.Tag.FirstGenre ?? "";
+                        }
+                        catch (Exception ex)
+                        {
+                            Console.WriteLine($"Error getting metadata for {songFile}: {ex.Message}");
+                        }
+
+                        songs += $"{eyo}##{title}##{artist}##{album}##{genre};";
+                    }
+                    songs = songs.Substring(0, songs.Length - 1);
+                    Console.WriteLine($"<LIST>{songs}<EOS>");
+                    tcpServer.Send(clientId, $"<LIST>{songs}<EOS>");
+                }
+            }
+		}
+
+    }
+}
---
+++ audio-server/audio-server/TCPServer.cs
@@ -0,0 +1,182 @@
+﻿namespace audio_server
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Net;
+	using System.Net.Sockets;
+	using System.Text;
+	using System.Threading;
+
+	public class TCPServer
+	{
+		private TcpListener listener;
+		private List<ClientHandler> clients = new List<ClientHandler>();
+
+		public event Action<string, string> DataReceived;
+
+
+		public TCPServer()
+		{
+			IPAddress address = IPAddress.Any;
+			listener = new TcpListener(address, 7777);
+		}
+
+		public void Start()
+		{
+			listener.Start();
+			Console.WriteLine("Server started...");
+
+			// Start accepting client connections asynchronously
+			listener.BeginAcceptTcpClient(HandleIncomingConnection, null);
+		}
+
+		private void HandleIncomingConnection(IAsyncResult ar)
+		{
+			TcpClient client = listener.EndAcceptTcpClient(ar);
+
+			// Create a new ClientHandler for this client
+			ClientHandler handler = new ClientHandler(client);
+			handler.DataReceived += HandleDataReceived;
+
+			// Add the client to the list
+			clients.Add(handler);
+
+			Console.WriteLine("connection incoming: " + client.Client.RemoteEndPoint.ToString());
+
+
+			// Start listening for data from this client
+			handler.Start();
+
+			// Accept the next client connection
+			listener.BeginAcceptTcpClient(HandleIncomingConnection, null);
+		}
+
+		private void HandleDataReceived(string clientId, string data)
+		{
+			DataReceived?.Invoke(clientId, data);
+		}
+
+
+		public void Send(string clientId, string data)
+		{
+			var client = clients.Find(c => c.ClientId == clientId);
+			if (client != null)
+			{
+				client.Send(data);
+			}
+			else
+			{
+				Console.WriteLine($"Client with ID {clientId} not found.");
+			}
+		}
+
+		public void Stop()
+		{
+			foreach (var client in clients)
+			{
+				client.Disconnect();
+			}
+			clients.Clear();
+			listener.Stop();
+		}
+	}
+
+	public class ClientHandler
+	{
+		private TcpClient client;
+		private NetworkStream stream;
+		private Thread receiveThread;
+
+		public string ClientId { get; private set; }
+		public string ClientIp { get; private set; }
+
+		public event Action<string, string> DataReceived;
+		public event Action<string> ClientDisconnected;
+
+		public ClientHandler(TcpClient tcpClient)
+		{
+			client = tcpClient;
+			stream = client.GetStream();
+			ClientId = Guid.NewGuid().ToString();
+			ClientIp = client.Client.RemoteEndPoint.ToString();
+		}
+
+		public void Start()
+		{
+			receiveThread = new Thread(Receive);
+			receiveThread.Start();
+		}
+
+		public void Send(string data)
+		{
+			byte[] buffer = Encoding.UTF8.GetBytes(data + "\r\n");
+			try
+			{
+				stream.Write(buffer, 0, buffer.Length);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error sending data: {ex.Message}");
+			}
+		}
+
+
+		private void Receive()
+		{
+			byte[] buffer = new byte[8192];
+			StringBuilder receivedData = new StringBuilder();
+			string remainingData = string.Empty;
+
+			while (client.Connected)
+			{
+				try
+				{
+					
+					int bytesRead = stream.Read(buffer, 0, buffer.Length);
+					if (bytesRead > 0)
+					{
+						string receivedText = remainingData + Encoding.UTF8.GetString(buffer, 0, bytesRead);
+						
+						// Split received text into messages based on "\r\n"
+						string[] messages = receivedText.Split(new string[] { "\r\n" }, StringSplitOptions.None);
+
+						// Process complete messages, excluding the last (potentially incomplete) message
+						for (int i = 0; i < messages.Length - 1; i++)
+						{
+							DataReceived?.Invoke(ClientId, messages[i]);
+						}
+
+						// Store any incomplete message for the next iteration
+						remainingData = messages[messages.Length - 1];
+
+						Array.Clear(buffer, 0, buffer.Length);
+					}
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Error receiving data: {ex.Message}");
+					Disconnect();
+				}
+			}
+
+			Disconnect();
+		}
+
+
+
+		public void Disconnect()
+		{
+			try
+			{
+				stream.Close();
+				client.Close();
+				ClientDisconnected?.Invoke(ClientId);
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error disconnecting client: {ex.Message}");
+			}
+		}
+	}
+
+}
